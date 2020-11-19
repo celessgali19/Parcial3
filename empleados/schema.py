33b271d1eadfd7aa520e15f8da2cfe485982001c
Celeste Galicia <celes0_33@hotmail.es>
2020-11-18 18:43:10 -0600

    Se realizo el create en el backend

--- empleados/schema.py
+++ empleados/schema.py
@@ -1,9 +1,9 @@
 import graphene
 from graphene import relay, ObjectType
-from graphene_django import DjangoObjectType
+from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
-from empleados.models import Puesto, Empleados
+from .models import Puesto, Empleados
 
 class PuestoNode(DjangoObjectType):
     class Meta:
@@ -24,9 +24,28 @@ class EmpleadosNode(DjangoObjectType):
         interfaces = (relay.Node, )
 
 
-class Query(graphene.ObjectType):
+class Query(object):
     puesto = relay.Node.Field(PuestoNode)
     all_puestos = DjangoFilterConnectionField(PuestoNode)
 
     empleados = relay.Node.Field(EmpleadosNode)
     all_empleados = DjangoFilterConnectionField(EmpleadosNode)
+
+    #crear empleado
+class CreatePuesto(graphene.Mutation):
+
+    class Arguments:
+        
+        nombre = graphene.String()
+
+    puesto = graphene.Field(PuestoNode)
+
+    def mutate(self, info, nombre):
+        puesto = Puesto.objects.create(nombre=nombre)
+     
+        return CreatePuesto(puesto=puesto)
+
+
+
+class Mutation(graphene.ObjectType):
+  create_puesto = CreatePuesto.Field()
--- proyecto/schema.py
+++ proyecto/schema.py
@@ -6,5 +6,6 @@ import empleados.schema
 class Query(empleados.schema.Query, graphene.ObjectType):
 
     pass
-
-schema = graphene.Schema(query=Query)
+class Mutation(empleados.schema.Mutation, graphene.ObjectType):
+    pass
+schema = graphene.Schema(query=Query, mutation=Mutation)
